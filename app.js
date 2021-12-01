dd3be5b3a0fbef6112c2dd363dcc4fd2e6497228
andreassenmarvin <marvinandreassen@gmail.com>
2021-12-02 01:29:27 +0300

    Create slide toggle menu

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-$(function () {
-    $("body").niceScroll();
-})
+var menuBar = document.querySelector(".menu-bar");
+var sideBar = document.querySelector(".side-bar");
+var overlay = document.querySelector("overlay2");
+
+menuBar.addEventListener("click", openMenu);
+overlay.addEventListener("click", closeMenu);
+
+function openMenu() {
+    menuBar.classList.toggle("click");
+    sideBar.classList.toggle("side-bar-visible")
+}
+
+function closeMenu() {
+    menuBar.classList.remove("hover");
+    sideBar.classList.remove("side-bar-visible");
+}

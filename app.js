5bc83713edfbe2c26c3b73c0a7915b9b76b912c8
andreassenmarvin <marvinandreassen@gmail.com>
2021-12-03 00:07:27 +0300

    Add profile typing text

--- app.js
+++ app.js
@@ -5,21 +5,52 @@ var bar1 = document.querySelector(".bar-1");
 var bar2 = document.querySelector(".bar-2");
 var bar3 = document.querySelector(".bar-3");
 
+
+var i = 0;
+var intro = "My name is Marvin Macharia, a phenomenal web and mobile applications developer." ;
+var introHead = document.getElementById("intro");
+var speed = 50;
+
+
 menuBar.addEventListener("click", openMenu);
 overlay.addEventListener("click", closeMenu);
 
 function openMenu() {
-    menuBar.classList.toggle("click");
-    sideBar.classList.toggle("side-bar-visible")
+    if (menuBar.classList.contains("click")) {
+        menuBar.classList.remove("click");
+        sideBar.classList.remove("side-bar-visible");
+        overlay.style.display = "none";
+    }
+
+    else {
+        menuBar.classList.add("click");
+        sideBar.classList.add("side-bar-visible");
+        overlay.style.display = "block";
+    }
 }
 
 function closeMenu() {
-    menuBar.classList.remove("hover");
+    menuBar.classList.remove("click");
     sideBar.classList.remove("side-bar-visible");
+    overlay.style.display = "none";
 }
 
 function animation() {
-    bar1.classList.add("bar-1-glow");
-    bar2.classList.add("bar-2-glow");
-    bar3.classList.add("bar-3-glow");
+    bar1.classList.add("menu-bar-hover");
+    bar2.classList.add("menu-bar-hover");
+    bar3.classList.add("menu-bar-hover");
+}
+
+function noAnimation() {
+    bar1.classList.remove("menu-bar-hover");
+    bar2.classList.remove("menu-bar-hover");
+    bar3.classList.remove("menu-bar-hover");
+}
+
+function typing() {
+    if (i < intro.length) {
+        introHead.innerHTML += intro.charAt(i);
+        i++;
+        setTimeout(typing, speed);
+    }
 }

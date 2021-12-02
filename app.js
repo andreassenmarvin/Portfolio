41aee87b83c9f9fcdc9a445be3485f0753c5b754
andreassenmarvin <marvinandreassen@gmail.com>
2021-12-02 03:06:48 +0300

    Add animation to menu bar

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var menuBar = document.querySelector(".menu-bar");
 var sideBar = document.querySelector(".side-bar");
-var overlay = document.querySelector("overlay2");
+var overlay = document.querySelector(".overlay2");
+var bar1 = document.querySelector(".bar-1");
+var bar2 = document.querySelector(".bar-2");
+var bar3 = document.querySelector(".bar-3");
 
 menuBar.addEventListener("click", openMenu);
 overlay.addEventListener("click", closeMenu);
@@ -13,4 +16,10 @@ function openMenu() {
 function closeMenu() {
     menuBar.classList.remove("hover");
     sideBar.classList.remove("side-bar-visible");
+}
+
+function animation() {
+    bar1.classList.add("bar-1-glow");
+    bar2.classList.add("bar-2-glow");
+    bar3.classList.add("bar-3-glow");
 }
